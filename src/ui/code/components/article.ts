deef8e63010a6e112078ad1ddbf5ec209fe6a660
Alexander Fritsch <alex@targoninc.com>
2026-01-29 16:23:26 +0100

    fixes

--- src/ui/code/components/article.ts
+++ src/ui/code/components/article.ts
@@ -13,30 +13,47 @@ export class Article {
             .html(html)
             .build();
 
-        for (let codeInPres of content.querySelectorAll("pre code")) {
-            const pre = codeInPres.parentElement;
-            if (!pre) {
-                continue;
+        function update() {
+            for (let a of content.querySelectorAll("a")) {
+                if (a.innerText.trim().startsWith(">")) {
+                    a.classList.add("citation");
+                    a.innerText = a.innerText.trim().substring(1);
+                }
             }
-            const code = codeInPres as HTMLPreElement;
 
-            pre.classList.add("flex-v", "rendered");
-            if (code.innerText.trim().startsWith("{{md:execute-js}}")) {
-                const trimmed = code.innerText.replaceAll("{{md:execute-js}}", "").trim();
-                const result = eval(`"use strict";(() => {${trimmed}})()`);
-                codeInPres.innerHTML = "";
-                codeInPres.classList.add("rendered");
-                pre.insertBefore(create("span")
-                    .classes("rendered_text")
-                    .text("Rendered").build(), pre.firstChild);
-                codeInPres.appendChild(result);
-            } else {
-                pre.insertBefore(Generics.button("content_copy", "Copy code", () => {
-                    navigator.clipboard.writeText(code.innerText);
-                }), pre.firstChild);
-                hljs.highlightElement(code);
+            const h1 = content.querySelector("h1");
+            if (h1) {
+                document.title = h1.innerText.trim();
+            }
+
+            for (let codeInPres of content.querySelectorAll("pre code")) {
+                const pre = codeInPres.parentElement;
+                if (!pre) {
+                    continue;
+                }
+                const code = codeInPres as HTMLPreElement;
+
+                pre.classList.add("flex-v", "rendered");
+                if (code.innerText.trim().startsWith("{{md:execute-js}}")) {
+                    const trimmed = code.innerText.replaceAll("{{md:execute-js}}", "").trim();
+                    const result = eval(`"use strict";(() => {${trimmed}})()`);
+                    codeInPres.innerHTML = "";
+                    codeInPres.classList.add("rendered");
+                    pre.insertBefore(create("span")
+                        .classes("rendered_text")
+                        .text("Rendered").build(), pre.firstChild);
+                    codeInPres.appendChild(result);
+                } else {
+                    pre.insertBefore(Generics.button("content_copy", "Copy code", () => {
+                        navigator.clipboard.writeText(code.innerText);
+                    }), pre.firstChild);
+                    hljs.highlightElement(code);
+                }
             }
         }
+
+        html.subscribe(update);
+        update();
 
         return content;
     }
