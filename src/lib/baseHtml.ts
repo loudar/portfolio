38824ff8bb527784794fd0484e1a1c50efe1c604
Alexander Fritsch <alex@targoninc.com>
2026-01-30 09:36:03 +0100

    add favicon

--- src/lib/baseHtml.ts
+++ src/lib/baseHtml.ts
@@ -25,6 +25,9 @@ export async function baseHtml(req: Request, hitsData: Record<string, number>) {
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="/styles/style.css"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/an-old-hope.min.css" rel="stylesheet">
+    
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/img/favicon.jpg">
 
     <!-- OG Tags -->
     <meta property="og:type" content="website"/>
